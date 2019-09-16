f677f7309ad8f51ee42975b65b29b00e875aa77d
delfinopaulaemerson <delfs@hotmail.com>
2019-09-16 15:21:57 -0300

    Inserção de usuário com POST

--- src/main/java/com/workshopmongo/curso/resources/UserResources.java
+++ src/main/java/com/workshopmongo/curso/resources/UserResources.java
@@ -1,5 +1,6 @@
 package com.workshopmongo.curso.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collector;
@@ -9,9 +10,11 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.workshopmongo.curso.domain.User;
 import com.workshopmongo.curso.dto.UserDTO;
@@ -44,6 +47,16 @@ public class UserResources {
 		user = this.service.findById(id);
 		
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+		User obj = this.service.formDto(objDTO);
+		
+		obj = this.service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/workshopmongo/curso/service/UserService.java
+++ src/main/java/com/workshopmongo/curso/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshopmongo.curso.domain.User;
+import com.workshopmongo.curso.dto.UserDTO;
 import com.workshopmongo.curso.repository.UserRepository;
 import com.workshopmongo.curso.service.exception.ObjectNotFoundException;
 
@@ -26,5 +27,15 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	
+	public User insert(User obj) {
+		
+		return this.repository.save(obj);
+	}
+	
+	public User formDto(UserDTO dto) {
+		return new User(dto.getId(), dto.getName(), dto.getEmail()); 
+	}
 
 }
